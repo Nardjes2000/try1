f22204d04047c2ef3e39622fa36497e11fdb1ca5
hn_amieur@esi.dz <hn_amieur@esi.dz>
2019-11-19 15:06:08 +0100

    pour telecherger fichier

--- app/Http/Controllers/usercontroller.php
+++ app/Http/Controllers/usercontroller.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
 use App\Client;
 
 class usercontroller extends Controller
@@ -15,7 +17,7 @@ class usercontroller extends Controller
     return view('welcome')->with('data',$data);
 
     }
-    public function store(Request $request)
+    public function store1(Request $request)
 
     {
 
@@ -32,4 +34,14 @@ $Client->name=$nom;
 $Client->age=$age;
 $Client->save();
 return back();}
+public function store(Request $request)
+    {//get current time and append the upload file extension to it,
+  //then put that name to $photoName variable.
+  $photoName = time().'.'.$request->user_photo->getClientOriginalExtension();
+
+  /*
+  talk the select file and move it public directory and make avatars
+  folder if doesn't exsit then give it that unique name.
+  */
+  $request->user_photo->move(public_path('avatars'), $photoName);}
 }
